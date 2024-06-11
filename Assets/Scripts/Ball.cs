c00361272bf95dada55c8a1f87675c3d8409e0d2
francorivaas <francoagustinrivas@hotmail.com>
2024-06-11 09:47:22 -0300

    we review the code and find some optimization patterns I

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -15,40 +15,56 @@ public class Ball : MonoBehaviour
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
     }
+
     void Start()
     {
+        //acá tenemos un ejemplo de precomputation
+        //ya que estamos realizando operaciones previo
+        // a usar los valores que asignamos
         gameManager = GameManager.Instance;
         startPosition = transform.position;
         ResetBall();
     }
+
     void Update()
     {
         if (gameManager.lives > 0) transform.position += velocity * Time.deltaTime;
     }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("DeadZone")) 
-        { 
-            gameManager.LoseHealth();
-            ResetBall();
+        //acá tenemos un caso de "expected path"
+        //puesto que, en orden de prioridad,
+        //esperamos que el output más común sea colisionar con los ladrillos
+        //luego con la paleta,
+        //finalmente las dead zones
+
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            Vector3 normal = collision.contacts[0].normal;
+            velocity = Vector3.Reflect(velocity, normal);
         }
+
         else if (collision.gameObject.CompareTag("Player"))
         {
             float hitFactor = (transform.position.x - collision.transform.position.x) / collision.collider.bounds.size.x;
             Vector3 direction = new Vector3(hitFactor, 1, 0).normalized;
             velocity = direction * speed;
         }
-        else if (collision.gameObject.CompareTag("Brick"))
-        {
-            Vector3 normal = collision.contacts[0].normal;
-            velocity = Vector3.Reflect(velocity, normal);
+
+        else if (collision.gameObject.CompareTag("DeadZone")) 
+        { 
+            gameManager.LoseHealth();
+            ResetBall();
         }
+
         else
         {
             Vector3 normal = collision.contacts[0].normal;
             velocity = Vector3.Reflect(velocity, normal);
         }
     }
+
     public void ResetBall()
     {
         transform.position = startPosition;
